eb4cdedb7c92b9a67fdd9cad6d57b1f1803d84c9
HassenMahdi <hassenmahdi77@gmail.com>
2020-07-15 20:41:39 +0200

    Abstract classes

--- app/main/service/check_service.py
+++ app/main/service/check_service.py
@@ -14,6 +14,7 @@ def get_domain_checks(domain_id):
 
     return checks
 
+
 def get_check_from_shared(folder_name='default'):
 
     default_checks: ShareDirectoryClient = service.get_directory_client(folder_name)
