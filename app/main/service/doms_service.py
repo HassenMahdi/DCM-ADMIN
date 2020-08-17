4ea6cecfd0ea01bae65f470d82ce2fb51eb891e3
HassenMahdi <hassenmahdi77@gmail.com>
2020-08-17 13:19:47 +0200

    Typo

--- app/main/service/doms_service.py
+++ app/main/service/doms_service.py
@@ -9,6 +9,7 @@ from app.db.Models.super_domain import SuperDomain
 def get_domain(dom_id):
     return Domain(id=dom_id).load()
 
+
 def save_domain(data):
     super_domain_id = data['super_domain_id']
     super_dom = SuperDomain(**{'id':super_domain_id}).load()
