95215c1109a976be4ed5edd426b97703c7011940
BHM-Yassine <yassine.bouhm@edu.uiz.ac.ma>
2021-08-17 22:40:06 +0100

    Add endpoint to Fetch all rsu composition

--- app/main/controller/rus_controller.py
+++ app/main/controller/rus_controller.py
@@ -1,16 +1,27 @@
 from flask import request
 from flask_restplus import Resource
 
-from ..service.rsu_service import import_rsu_data_from_file
+from ..service.rsu_service import import_rsu_data_from_file, get_all_rsu_data
 from ..util.decorator import token_required
 from ..util.dto import RsuDto
 
 api = RsuDto.api
+rsu_data = RsuDto.rsu_data
+rsu_with_header = RsuDto.rsu_with_header
+
+
+@api.route('/')
+class RefDataList(Resource):
+    @token_required
+    @api.doc('Get All RSU Composition')
+    @api.marshal_list_with(rsu_with_header)
+    def get(self):
+        return get_all_rsu_data()
 
 
 @api.route('/import')
 class RefData(Resource):
-    @api.doc('import ref')
+    @api.doc('import rsu composition data')
     @api.response(201, 'RSU composition successfully imported.')
     @token_required
     def post(self):
