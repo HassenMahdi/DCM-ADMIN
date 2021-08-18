8e5dee8a67a6063a0bf87b37d5f43a4f47e973cc
BHM-Yassine <yassine.bouhm@edu.uiz.ac.ma>
2021-08-18 13:02:51 +0100

    Add check file extension allowed

--- app/main/util/rsu_utils.py
+++ app/main/util/rsu_utils.py
@@ -1,21 +1,20 @@
-sources = ["Nom", "Adresse", "Type logement", "Nombre de pieces", "Ordinateur",
+SOURCES = ["Nom", "Adresse", "Type logement", "Nombre de pieces", "Ordinateur",
           "Niveau scolaire du conjoint", "Sanitaire", "Antenne parabolique"]
 
-targets = ["nom", "adresse", "type_logement", "nb_pieces", "oridnateur", "nv_scolaire_conjoint",
+TARGETS = ["nom", "adresse", "type_logement", "nb_pieces", "oridnateur", "nv_scolaire_conjoint",
           "sanitaire", "antenne_parabolique"]
+
+ALLOWED_EXTENSIONS = {"xlsx", "xlx"}
+# "xlmx", "csv"
 
 
 def rsu_map_column(column):
-    # mapping = {"Nom": "nom",
-    #            "Adresse": "adresse",
-    #            "Type logement": "type_logement",
-    #            "Nombre de pieces": "nb_pieces",
-    #            "Ordinateur": "oridnateur",
-    #            "Niveau scolaire du conjoint": "nv_scolaire_conjoint",
-    #            "Sanitaire": "sanitaire",
-    #            "Antenne parabolique": "antenne_parabolique",
-    #            }
-
-    mapping = dict(zip(sources, targets))
-
+    mapping = dict(zip(SOURCES, TARGETS))
     return mapping.get(column)
+
+
+def allowed_file(filename):
+    file_extension = filename.rsplit('.', 1)[-1].lower()
+
+    if '.' in filename and file_extension in ALLOWED_EXTENSIONS:
+        return file_extension
